ceae49f0eaf347efd3ccd82b3250a1b0b59fe47b
Yaervig <145300964+Yaervig@users.noreply.github.com>
2025-10-01 17:18:51 -0400

    Profile Creation

---
+++ hdr/profile.h
@@ -0,0 +1,51 @@
+#ifndef PROFILEH
+#define PROFILEH
+
+#include <string>
+#include <ctime>
+
+struct date
+{
+	int day;
+	int month;
+	int year;
+};
+
+class Profile
+{
+private:
+	std::string username;
+	std::string userpass;
+	std::string f_pfp;
+	std::string bio;
+
+	date creationDate;
+	date birthday;
+
+	unsigned int uid;
+	int interests;
+
+public:
+	Profile();
+
+	std::string getUsername(void);
+	std::string getUserpass(void);
+	std::string getProfilePicture(void);
+	std::string getBio(void);
+
+	date getCreationDate(void);
+	date getBirthday(void);
+
+	int getUid(void);
+	int getInterests(void);
+
+	bool setUsername(std::string newUsername);
+	bool setUserpass(std::string newUserpass);
+	bool setProfilePicture(std::string newProfilePicture);
+	bool setBio(std::string newBio);
+
+	void setCreationDate(void);
+	bool setBirthday(date newBirthday);
+};
+
+#endif
