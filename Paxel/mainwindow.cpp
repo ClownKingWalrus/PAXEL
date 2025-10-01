fdfd8285399bb2232d13d5143dec407a524e3824
sdo05 <crazycat0635@gmail.com>
2025-09-30 20:38:54 -0400

    Adding QT project to Github repo

---
+++ Paxel/mainwindow.cpp
@@ -0,0 +1,32 @@
+#include "mainwindow.h"
+#include "ui_homescreen.h"
+#include "ui_mainwindow.h"
+#include "signup.h"
+#include "ui_signup.h"
+#include "homescreen.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    SignUp *SignUpSignUp = new SignUp;
+    HomeScreen *HomeScreenHomeScreen = new HomeScreen;
+
+    connect(ui->SignUp1,SIGNAL(clicked(bool)),this,SLOT(hide()));
+    connect(ui->SignUp1,SIGNAL(clicked(bool)),SignUpSignUp,SLOT(show()));
+
+    connect(SignUpSignUp->ui->Login2,SIGNAL(clicked(bool)),SignUpSignUp,SLOT(hide()));
+    connect(SignUpSignUp->ui->Login2,SIGNAL(clicked(bool)),this,SLOT(show()));
+
+    connect(ui->Login1,SIGNAL(clicked(bool)),this,SLOT(hide()));
+    connect(ui->Login1,SIGNAL(clicked(bool)),HomeScreenHomeScreen,SLOT(show()));
+
+    connect(HomeScreenHomeScreen->ui->LogOut,SIGNAL(clicked(bool)),HomeScreenHomeScreen,SLOT(hide()));
+    connect(HomeScreenHomeScreen->ui->LogOut,SIGNAL(clicked(bool)),this,SLOT(show()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
