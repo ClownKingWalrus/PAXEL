98f4bb6b8753cba19f70b0ce134cbe2f27988715
Trunks <buttermunchinwalrus@gmail.com>
2025-10-14 19:24:09 -0400

    [#POF_43] Touched up signup a bit

--- Paxel/main.cpp
+++ Paxel/main.cpp
@@ -13,11 +13,13 @@ int main(int argc, char *argv[])
     //DiscussionBoard w;
     //w.show();
 
-     ThreadMenuWindow test(nullptr, "IYGS9O9EVDZVPLF");
-     test.show();
+     //ThreadMenuWindow test(nullptr, "IYGS9O9EVDZVPLF");
+     //test.show();
 
     //RepliesWindow test(nullptr, "2T2EF1CIGLX7H2D");
     //test.show();
 
+    MainWindow w;
+    w.show();
     return a.exec();
 }
